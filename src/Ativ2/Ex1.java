3bf318a055db36368bec006785075697d663221a
cogeti <cogeti@cogeti-OptiPlex-7040>
2019-03-27 13:00:11 -0300

    Finalizando Ex3

--- src/Ativ2/Ex1.java
+++ src/Ativ2/Ex1.java
@@ -11,35 +11,45 @@ import java.util.Random;
 
 /**
  *
- * @author a1792334
+ * @author tuchinski
  */
 
-class MyThread extends Thread{
+class MyThreadRdn extends Thread{
     int value = 0;
-            
-    public MyThread(){
+    int id = -1;
+   
+     public MyThreadRdn(){
         Random aleatorio = new Random();
-        this.value = 1000 + aleatorio.nextInt(2000-1000);
-        
-        
+        this.value = 1000 + aleatorio.nextInt(2000-1000); 
+    }
+     
+    public MyThreadRdn(int id){
+        Random aleatorio = new Random();
+        this.id = id;
+        this.value = 1000 + aleatorio.nextInt(2000-1000); 
     }
     
     @Override
     public void run() {
         try {
             Thread.sleep(value);
+            
+            if(Thread.interrupted()){
+                throw new InterruptedException();
+            }
         } catch (InterruptedException ex) {
-            System.out.println("Thread interrupted");
+            System.out.println("MyThreadRdn interrupted");
+            return;
         }
-        System.out.println("Thread finalized in " + value + " seconds");
+        System.out.println("Thread " + this.id + " finalized in " + value + " seconds");
     }
-    
 }
 
 public class Ex1 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            new MyThread().start();
+        int numThreads = 10;
+        for (int i = 0; i < numThreads; i++) {
+            new MyThreadRdn(i).start();
         }
         
     }
