a7ca84df7af139b3d5585ec96d4c37dcb3d705ea
tuchinski <tuchinski@alunos.utfpr.edu.br>
2019-04-29 15:33:25 -0300

    iniciando exercico slide7

---
+++ src/slide7/ex1/BufferSemaforo.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slide7.ex1;
+
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author a1792334
+ */
+public class BufferSemaforo {
+    private int num;
+    private boolean isFull;
+    
+    private final Semaphore semaforo;
+
+    public BufferSemaforo(Semaphore semaforo) {
+        this.semaforo = semaforo;
+    }
+
+    
+    
+    
+    public synchronized int consomeBuffer(){
+        while(!this.isFull){
+            try {
+                semaforo.acquire();
+                this.isFull = false;
+            } catch (InterruptedException ex) {
+                Logger.getLogger(BufferSemaforo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return this.num;
+    }
+    
+    
+}
