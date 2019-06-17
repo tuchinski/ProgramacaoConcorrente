781636a59cc363e4a0d40e3b703eb8bc20fefe29
a1792334 <a1792334@05e104-507894>
2019-06-17 15:20:36 -0300

    start ex slide 10

---
+++ src/slide10/parte2/ex1/EncontraMaiorVetor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slide10.parte2.ex1;
+
+/**
+ *
+ * @author a1792334
+ */
+
+//int[] vetor = new int[10];
+public class EncontraMaiorVetor {
+    int numTarefas;
+    int numThreads;
+}
